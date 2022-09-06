82c343072b691e0e09096dc2775b7ac06f34d131
Verbovskiy Alexandr <cofeek5@gmail.com>
2022-09-06 11:08:49 +0300

    install dependenses

---
+++ client/src/components/App/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App bg-red-200">
+      <h1 className="underline">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
